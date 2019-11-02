3cfd00bc1923eea103852ba41b7e2c50c1731c25
Victor <2bravepickle@gmail.com>
2019-11-02 16:58:36 +0200

    fixing favicons & update styles

--- webpack.config.js
+++ webpack.config.js
@@ -46,6 +46,7 @@ Encore
     .enableSourceMaps(!Encore.isProduction())
     // enables hashed filenames (e.g. app.abc123.css)
     .enableVersioning(Encore.isProduction())
+    // .enableVersioning(true)
 
     // enables @babel/preset-env polyfills
     .configureBabel(() => {}, {
